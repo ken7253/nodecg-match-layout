a5a42ef4b3db07060fdd6e05022893f409c4aba1
ken7253 <ken7253@gmail.com>
2020-08-06 00:54:18 +0900

    add func 'isPerformanceMode'

--- dashboard/main.js
+++ dashboard/main.js
@@ -4,16 +4,28 @@ const acceptTeams = document.getElementById('acceptTeams');
 const switchTeams = document.getElementById('switchTeams');
 const roundCountAlfa = document.getElementById('roundAlfa').value;
 const roundCountBravo = document.getElementById('roundBravo').value;
+const performanceMode = document.getElementById('performanceCheckbox');
+
 // Replicant
 const teamsRep = nodecg.Replicant('teams');
 
+// Function
+function isPerformanceMode() {
+	return performanceMode.checked;
+}
+
+// EventListener
 acceptTeams.addEventListener('click', () => {
-	const teamAlfa = document.getElementById('teamAlfa').value;
-	const teamBravo = document.getElementById('teamBravo').value;
-	teamsRep.value = [teamAlfa, teamBravo];
+	if (isPerformanceMode()) {
+		alert('本番中に変更ができない項目です');
+	} else {
+		const teamAlfa = document.getElementById('teamAlfa').value;
+		const teamBravo = document.getElementById('teamBravo').value;
+		teamsRep.value = [teamAlfa, teamBravo];
+	}
 });
 
 switchTeams.addEventListener('click', () => {
 	teamsRep.value.reverse();
 });
-// Function
+// ラウンド設定追加する。
