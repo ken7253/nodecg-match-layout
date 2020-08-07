994191c4d50f539303e380ef023a7d98556f8532
ken7253 <ken7253@gmail.com>
2020-08-08 02:58:35 +0900

    create round controller

--- dashboard/main.js
+++ dashboard/main.js
@@ -2,9 +2,11 @@
 // DOM
 const acceptTeams = document.getElementById('acceptTeams');
 const switchTeams = document.getElementById('switchTeams');
-const roundCountAlfa = document.getElementById('roundAlfa').value;
-const roundCountBravo = document.getElementById('roundBravo').value;
+const acceptRounds = document.getElementById('acceptRound');
+const resetRounds = document.getElementById('resetRound');
+const roundLimit = document.getElementById('roundLimit');
 const performanceMode = document.getElementById('performanceCheckbox');
+const PERFORMANCE_MODE_ERR = "本番中に変更できない項目です\n変更する場合は本番モードを解除してください。";
 
 // Replicant
 const teamsRep = nodecg.Replicant('teams');
@@ -12,12 +14,36 @@ const teamsRep = nodecg.Replicant('teams');
 // Function
 function isPerformanceMode() {
 	return performanceMode.checked;
-}
+};
+
+function createRoundDOM(round) {
+	// need refactoring this function :)
+	const tagetAlfa = document.getElementById('roundAlfa');
+	const tagetBravo = document.getElementById('roundBravo')
+	tagetAlfa.textContent = null;
+	tagetBravo.textContent = null;
+	for (let i = 0; i < round; i++) {
+		const optionEl = document.createElement('option');
+		const textContent = document.createTextNode(i);
+		optionEl.setAttribute('value', i);
+		optionEl.appendChild(textContent);
+		tagetAlfa.appendChild(optionEl);
+		// add one loop 
+	};
+	for (let i = 0; i < round; i++) {
+		const optionEl = document.createElement('option');
+		const textContent = document.createTextNode(i);
+		optionEl.setAttribute('value', i);
+		optionEl.appendChild(textContent);
+		tagetBravo.appendChild(optionEl);
+	};
+};
 
 // EventListener
+// TeamSetting
 acceptTeams.addEventListener('click', () => {
 	if (isPerformanceMode()) {
-		alert('本番中に変更ができない項目です');
+		alert(PERFORMANCE_MODE_ERR);
 	} else {
 		const teamAlfa = document.getElementById('teamAlfa').value;
 		const teamBravo = document.getElementById('teamBravo').value;
@@ -28,4 +54,20 @@ acceptTeams.addEventListener('click', () => {
 switchTeams.addEventListener('click', () => {
 	teamsRep.value.reverse();
 });
-// ラウンド設定追加する。
+
+// RoundSetting
+roundLimit.addEventListener('change', () => {
+	if(isPerformanceMode()) {
+		alert(PERFORMANCE_MODE_ERR);
+	} else {
+		createRoundDOM(roundLimit.value);
+	};
+});
+
+acceptRounds.addEventListener('click', () => {
+	const roundCountAlfa = document.getElementById('roundAlfa').value;
+	const roundCountBravo = document.getElementById('roundBravo').value;
+	console.log('Alfa:',roundCountAlfa,'Bravo:',roundCountBravo)
+	// input data => graphics
+
+});
