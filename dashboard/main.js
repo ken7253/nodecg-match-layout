87c4ff1aa1ead8db4e432b9937ce119797ef0d02
ken7253 <ken7253@gmail.com>
2020-08-09 15:25:03 +0900

    fix round count loop

--- dashboard/main.js
+++ dashboard/main.js
@@ -6,7 +6,7 @@ const acceptRounds = document.getElementById('acceptRound');
 const resetRounds = document.getElementById('resetRound');
 const roundLimit = document.getElementById('roundLimit');
 const performanceMode = document.getElementById('performanceCheckbox');
-const PERFORMANCE_MODE_ERR = "本番中に変更できない項目です\n変更する場合は本番モードを解除してください。";
+const PERFORMANCE_MODE_ERR = '本番中に変更できない項目です\n変更する場合は本番モードを解除してください。';
 
 // Replicant
 const teamsRep = nodecg.Replicant('teams');
@@ -17,20 +17,19 @@ function isPerformanceMode() {
 };
 
 function createRoundDOM(round) {
-	// need refactoring this function :)
+	// Need refactoring this function :)
 	const tagetAlfa = document.getElementById('roundAlfa');
-	const tagetBravo = document.getElementById('roundBravo')
+	const tagetBravo = document.getElementById('roundBravo');
 	tagetAlfa.textContent = null;
 	tagetBravo.textContent = null;
-	for (let i = 0; i < round; i++) {
+	for (let i = 0; i <= round; i++) {
 		const optionEl = document.createElement('option');
 		const textContent = document.createTextNode(i);
 		optionEl.setAttribute('value', i);
 		optionEl.appendChild(textContent);
 		tagetAlfa.appendChild(optionEl);
-		// add one loop 
 	};
-	for (let i = 0; i < round; i++) {
+	for (let i = 0; i <= round; i++) {
 		const optionEl = document.createElement('option');
 		const textContent = document.createTextNode(i);
 		optionEl.setAttribute('value', i);
@@ -57,17 +56,17 @@ switchTeams.addEventListener('click', () => {
 
 // RoundSetting
 roundLimit.addEventListener('change', () => {
-	if(isPerformanceMode()) {
+	if (isPerformanceMode()) {
 		alert(PERFORMANCE_MODE_ERR);
 	} else {
 		createRoundDOM(roundLimit.value);
-	};
+	}
 });
 
 acceptRounds.addEventListener('click', () => {
 	const roundCountAlfa = document.getElementById('roundAlfa').value;
 	const roundCountBravo = document.getElementById('roundBravo').value;
-	console.log('Alfa:',roundCountAlfa,'Bravo:',roundCountBravo)
-	// input data => graphics
-
+	console.log('Alfa:', roundCountAlfa, 'Bravo:', roundCountBravo);
+	// Input data => graphics
 });
+
